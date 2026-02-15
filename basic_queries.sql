ef0b28f8c0b075317a127b5dd680129ca4bd3be2
Charrishma Balla <charrishmmaballa@gmail.com>
2026-02-15 12:55:55 +0530

    Added basic SQL queries

---
+++ basic_queries.sql
@@ -0,0 +1,13 @@
+-- Create table
+CREATE TABLE student(
+  roll INT,
+  name VARCHAR(20),
+  dept VARCHAR(10)
+);
+
+-- Insert values
+INSERT INTO student VALUES(1,'Rahul','CSE');
+INSERT INTO student VALUES(2,'Anitha','ECE');
+
+-- Select all
+SELECT * FROM student;
